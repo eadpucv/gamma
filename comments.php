a65e6a325191071885a0fe018ce72f26536d85dd
Rodrigo Moya <rodrigo@citisent.com>
2014-07-02 21:37:09 -0300

    Comentarios (Funcionando, sin diseño de form)

--- comments.php
+++ comments.php
@@ -15,27 +15,14 @@
 				</div>									
 				<?php endforeach; ?>
 			<?php else : // If there are no comments yet ?>
-				<p><?php _e('No comments yet.'); ?></p>
+				<p><?php _e('Aun no hay comentarios en esta publicación'); ?></p>
 			<?php endif; ?>
 		</div>
 		<?php if ( comments_open() ) : ?>
+
 		<div class='col-lg-5 col-md-6 col-sm-12 col-xs-12'>
 			<div class='comentar'>
-				<form class='md' action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
-					<legend>Deja un comentario en esta noticia</legend>
-					<div class='grupo'>
-						<p><input type="text" name="author" id="author" value="<?php echo $comment_author; ?>" size="22" tabindex="1" />
-						<label for="author"><small><?php _e('Name'); ?> <?php if ($req) _e('(required)'); ?></small></label></p>
-
-						<p><input type="text" name="email" id="email" value="<?php echo $comment_author_email; ?>" size="22" tabindex="2" />
-						<label for="email"><small><?php _e('Mail (will not be published)');?> <?php if ($req) _e('(required)'); ?></small></label></p>
-
-						<p><input type="text" name="url" id="url" value="<?php echo $comment_author_url; ?>" size="22" tabindex="3" />
-						<label for="url"><small><?php _e('Website'); ?></small></label></p>      	
-					  	<textarea name="comment" id="comment" cols="100%" rows="10" tabindex="4"></textarea>
-					</div>
-					<input type='submit' class='btn btn-md gris' value='Comentar'>
-				</form>
+				<?php comment_form(array('title_reply'=>'Deja un comentario.','comment_notes_before' => ' ','comment_notes_after' => ' ',)); ?>
 			</div>
 		</div>
 		<?php endif; ?>
