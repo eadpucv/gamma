2ac920d3af4c625c6fad298d2e6e877e93426545
Rodrigo Moya <rodrigo@citisent.com>
2014-07-02 22:48:08 -0300

    Carrusel Home

--- includes/home/home_carousel.php
+++ includes/home/home_carousel.php
@@ -1,75 +1,42 @@
+<?php 
+  // ultimo posts de "Informacion de Docencia"
+  $cat_id = 1915; 
+  $carrusel_posts = new WP_Query( array('posts_per_page' => 5, 'category__in' => array($cat_id)));
+?>  
+<div class='alto-lg'>
+  <div data-ride="carousel" class="carousel slide" id="carousel-example-generic"> 
+    <ol class="carousel-indicators"> 
+      <li data-slide-to="0" data-target="#carousel-example-generic" class="active"></li> 
+      <li data-slide-to="1" data-target="#carousel-example-generic"></li>  
+    </ol> 
+ 
 <div class='alto-lg'>
   <div data-ride="carousel" class="carousel slide" id="carousel-example-generic"> 
     <ol class="carousel-indicators"> 
       <li data-slide-to="0" data-target="#carousel-example-generic" class="active"></li> 
       <li data-slide-to="1" data-target="#carousel-example-generic"></li> 
-      <li data-slide-to="2" data-target="#carousel-example-generic"></li> 
-      <li data-slide-to="3" data-target="#carousel-example-generic"></li>
-      <li data-slide-to="4" data-target="#carousel-example-generic"></li>  
     </ol> 
     <div class="carousel-inner"> 
-      <div class="item active imagen-carousel"> <!-- 1er slide -->
-        <img class='altura-maxima' alt="First slide" src="http://eadpucv.github.io/giornata/img/noticia-categoria-2.jpg" title="Noticia Destacada"> 
-        <div class='pie-de-foto'> 
-          <p class='centrado'><i class="icn icn-noticias icn-lg"></i> e[ad] presentó libro “Los Ojos del Gato & El Retoque Inacabado -Memorial de Edison Simons”</p> 
-        </div> 
-        <div class='wrap-carousel'> 
-          <div class='col-lg-3 oculto-sm oculto-xs cita-carousel'> 
-            <h1>Los Ojos del Gato & El Retoque Inacabado</h1> 
-            <p>El jueves 8 de mayo se llevó a cabo en la Escuela de Arquitectura y Diseño la inauguración de la exposición de grabados del poeta panameño Edison Simons, y el lanzamiento del libro “Los Ojos del Gato & El Retoque Inacabado -Memorial de Edison Simons”, del escritor brasileño Gerardo Mello Mourão.</p><span class='derecha'> Francesca Cambiaso </span>
-          </div>
-        </div> 
-      </div>
-      <div class="item imagen-carousel"> <!-- 2do slide -->
-        <img class='altura-maxima' alt="Second slide" src="http://oi62.tinypic.com/6tipls.jpg" title="Observación de la semana"> 
-        <div class='pie-de-foto'> 
-          <p class='centrado'><i class="icn icn-ojo icn-lg"></i> Observación de la semana realizada por el profesor David Jolly</p> 
-        </div> 
-        <div class='wrap-carousel'> 
-          <div class='col-lg-3 oculto-sm oculto-xs cita-carousel'> 
-            <h1>Observación "La Alcoba"</h1> 
-            <p>La Alcoba en la Ciudad Abierta.<br>
-            Se trata de un espacio mínimo para una familia inicial, su superficie de suelos es de treinta y seis metros cuadrados, (387,5 pies cuadrados), los que se aumentan con unos muros contenedores que reciben a la mayoría de los muebles.<br>
-            Lo edificado construye el exterior levantándose, reconstruye la arena como un libre paso sin interrumpir la extensión.</p> <span class='derecha'>David Jolly</span>  
-          </div>
-        </div> 
-      </div>
-      <div class="item imagen-carousel"> <!-- 3er slide -->
-        <img class='altura-maxima' alt="Third slide" src="http://eadpucv.github.io/giornata/img/historia7.jpg" title="Historia de la Escuela"> 
-        <div class='pie-de-foto'> 
-          <p class='centrado'><i class="icn icn-libro icn-lg"></i> Fundación de la Escuela</p>  
-        </div> 
-        <div class='wrap-carousel'> 
-          <div class='col-lg-3 oculto-sm oculto-xs cita-carousel'> 
-            <h1>Fundación de la Escuela</h1> 
-            <p>En 1952, un grupo de arquitectos y artistas (Alberto Cruz C. †, Godofredo Iommi †, Fabio Cruz P †, Miguel Eyquem, José Vial †, Arturo Baeza †, Francisco Méndez, Jaime Bellalta y Claudio Girola † (que llegó al poco tiempo) se incorporan a la Escuela de Arquitectura de la Universidad Católica de Valparaíso, fundando, simultáneamente, el Instituto de Arquitectura que sirve y alimenta a la Escuela.
-          </div>
-        </div> 
-      </div>
-      <div class="item imagen-carousel"> <!-- 4to slide -->
-        <img class='altura-maxima' alt="Fourth slide" src="https://farm5.staticflickr.com/4070/4562740335_36b9ce396f_b.jpg" title="Amereida"> 
-        <div class='pie-de-foto'> 
-          <p class='centrado'><i class="icn icn-acto icn-lg"></i> Amereida</p> 
-        </div> 
-        <div class='wrap-carousel'> 
-          <div class='col-lg-3 oculto-sm oculto-xs cita-carousel'> 
-            <h1>Amereida</h1> 
-            <p>Amereida es un poema que reune en su nombre el hallazgo de América y la épica latina del piadoso Eneas escrita en la Eneida. Dos años después de la Travesía del 65 se recolectaron textos diversos, anotaciones, poemas, cartas de los primeros cronistas americanos, blancos enteros, recortes y dibujos, y se realizó un trabajo de edición del material para darle forma a un libro titulado Amereida, sin firma de autor alguno, nada numerando sus páginas y ninguna mayúscula; la primera edición reza: amereida, volumen primero, colección poesía, Santiago, Chile; Editorial Cooperativa Lambda. Inscripción Nº 33306; el libro remata antes de la página de gracia con un 15 de mayo de 1967.</p> 
-          </div>
-        </div> 
-      </div>
-      <div class="item imagen-carousel"> <!-- 5to slide -->
-        <img class='altura-maxima' alt="Fifth slide" src="https://farm6.staticflickr.com/5216/5384568332_27b8fa9274_b.jpg" title="Campus"> 
-        <div class='pie-de-foto'> 
-          <p class='centrado'><i class="icn icn-mapa-mas icn-lg"></i> Campus: Ciudad Abierta</p> 
-        </div> 
-        <div class='wrap-carousel'> 
-          <div class='col-lg-3 oculto-sm oculto-xs cita-carousel'> 
-            <h1>Campus: Ciudad Abierta</h1> 
-            <p>Ciudad Abierta es un territorio cuyo espacio es construido por sus propios habitantes a través de los oficios; siempre bajo la luz de “Amereida”, poema épico que revela una visión poética de América. El territorio se ubica en en un sector de Ritoque llamado Punta Piedra, entre las comunas de Concón y Quintero en Valparaíso, Chile, a 4 kilómetros al norte de la desembocadura del río Aconcagua. Su extensión aproximada es de 240 hectáreas. Aparte de contar con un valor arquitectónico único, Ciudad Abierta alberga una biodiversidad en su ecosistema.</p>
-          </div>
-        </div> 
-      </div>     
+      <?php $i=0; ?>
+      <?php if( $carrusel_posts->have_posts() ) : while( $carrusel_posts->have_posts() ) : $carrusel_posts->the_post(); ?>        
+        <div class="item <?php if ($i == 0){ echo ' active '; $i=1; }?> imagen-carousel">
+          <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' ); 
+          if ($image) : ?>
+              <img src="<?php echo $image[0]; ?>" alt="" class="altura-maxima img_carrusel" />
+          <?php endif; ?> 
+
+          <div class='pie-de-foto'> 
+            <p class='centrado'><i class="icn icn-noticias icn-lg"></i><?php the_title(); ?></p> 
+          </div> 
+          <div class='wrap-carousel'> 
+            <div class='col-lg-3 oculto-sm oculto-xs cita-carousel'> 
+              <h1><?php the_title(); ?></h1> 
+              <?php the_excerpt() ?>
+              <span class='derecha'> Francesca Cambiaso </span>
+            </div>
+          </div> 
+        </div>
+      <?php endwhile; endif; ?>     
     </div>
   </div>
-</div>  
+</div> 
