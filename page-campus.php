bac53797ebc065ab17db53d23e68b46d0ba3cb0a
Idar <idargonzalez@gmail.com>
2014-07-04 14:16:49 -0400

    the_content

--- page-campus.php
+++ page-campus.php
@@ -51,7 +51,80 @@
                     </div>
                 </div>
             </div>
-            <?php the_content();  ?>
+            <div class='fila'>
+    <div class='fondo-blanco'>
+        <div class='wrap'>
+            <div class='historia'>
+                <h1 class='rojo-claro'>Escuela de Arquitectura y Diseño, Casa de estudio</h1>
+                <h4 class='subtitulo gris'>Avenida Matta 12, Recreo, Viña del Mar</h4>
+                <div class='bloque'>
+                    <article class="h-entry especifico">
+                        <div class="e-content p-summary p-name">
+                            <div class='bloque'>
+                                <p>La Facultad de Arquitectura y Diseño de la Pontificia Universidad Católica de Valparaíso,1 también conocida en Chile como la "Escuela de Arquitectura y Diseño de la Pontificia Universidad Católica de Valparaíso",2 "e[ad] Escuela de Arquitectura y Diseño",3 la "Escuela de Valparaíso",4 o simplemente "La Escuela",5 corresponde a una de la facultades de la Pontificia Universidad Católica de Valparaíso. En ella se imparten las carreras de Arquitectura, Diseño Gráfico y Diseño Industrial. Recientemente imparte programas de postgrado centrado en el "diseño náutico y marítimo" y en "ciudad y territorio".</p>
+                                <p>Siendo fuertemente influenciada por las teorías de Le Corbusier, desde fines de los años 60 se establece como una de las escuelas de Arquitectura Chilena más vanguardistas e innovadoras al postular una relación directa entre la Arquitectura y la Poesía, además de asignarle un papel fundamental a la observación en la creación arquitectónica.</p>
+                                <p>Es una de las escuelas de Arquitectura más antiguas de Chile, y dentro de ella se gestó la fundación de la Cooperativa Amereida. Comprende también las carreras de diseño gráfico y diseño industrial, las que son impartidas bajo el mismo punto de vista (o según lo denomina la facultad, bajo el mismo ethos) de innovación que la carrera de arquitectura.</p>
+                                <p>En el año 1952, la facultad contrató al arquitecto chileno Alberto Cruz y al poeta argentino Godofredo Iommi (entre otros), quienes imprimieron su particular visión de la arquitectura, y formularon las bases, que generaron el desarrollo actual de la aproximación a la arquitectura que presenta la facultad.</p>
+                                <p>En el ámbito social y político, en esta escuela se realizó la primera movilización a nivel nacional del proceso conocido como Reforma Universitaria de 1967, cuando el 15 de junio de ese año los estudiantes iniciaron una toma, con el apoyo decidido de los docentes, solicitando cogobierno, "universidad abierta" y que el rector fuere elegido por los académicos, no por la jerarquía católica. El movimiento alcanzó sus principales objetivos en pocos días, y fue replicado en muchas otras universidades del país en los años siguientes.</p>
+<iframe width='100%' height='500px' frameBorder='0' src='https://a.tiles.mapbox.com/v3/idar.im6c5nep/attribution,zoompan,geocoder.html'></iframe>
+                            </div>
+                        </div>
+                    </article> 
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class='fila'>
+    <div class='fondo-oscuro'>
+        <div class='wrap'>
+            <div class='historia'>
+                <h1 class='rojo-claro'>Ciudad Abierta</h1>
+                <h4 class='subtitulo gris'>Km.4 Camino a Concón - Quintero, Sector Punta Piedra</h4>
+                <div class='bloque'>
+                    <article class="h-entry especifico">
+                        <div class="e-content p-summary p-name">
+                            <div class='bloque'>
+                                <p class='gris-claro'>En 1969, los profesores de la facultad, fundaron una cooperativa privada, hoy conocida como Corporación Cultural Amereida, la cual adquirió un extenso terreno en el sector de Punta de Piedra, ubicada entre las localidades de Con-cón y Quintero. Dicho terreno fue bautizado como Ciudad Abierta y se convirtió en un campo de experimentación arquitectónico, donde se construyeron (y se siguen construyendo), proyectos experimentales de arquitectura, utilizando materiales y formas poco convencionales, siendo ejecutados principalmente por los alumnos y profesores de la facultad.</p>
+                                <p class='gris-claro'>Si bien, la Corporación Cultural Amereida y la facultad, en estricto rigor, son entidades independientes entre sí, la estrecha relación que poseen, dado que la mayoría de los profesores de la facultad son miembros de la corporación, y los trabajos que en la Ciudad Abierta realizan los alumnos y profesores, vuelve prácticamente imposible hablar de una sin mencionar a la otra, ya que ambas establecen una relación simbiótica.</p>
+                                <p class='gris-claro'>La arquitectura realizada y fomentada por la facultad, ha producido en la "Ciudad Abierta" obras controvertidas, tanto por el uso de los materiales, como por la forma y características de los espacios creados, un ejemplo son las "Hospederías" (denominadas así por la misma facultad), cuyo estilo ha sido también catalogado como deconstructivismo, arquitectura orgánica y arquitectura regionalista, sin embargo, debido a que su génesis no aloja nada en común con estos movimientos, sino que aboga por un aproximación completamente diferente a la arquitectura, la comparación no ha podido ser fundamentada con éxito.</p>
+                                <p class='gris-claro'>Ya desde 1970, la facultad, en relación con la corporación cultural, ha ejercido una influencia dentro de los círculos de arquitectura, tanto chilenos como internacionalesa, debido a su original y radical punto de vista respecto a la aproximación al diseño arquitectónico, la enseñanza de la arquitectura y la relación del arquitecto con la obra.</p>
+                                <p class='gris-claro'>Ciudad Abierta es un territorio cuyo espacio es construido por sus propios habitantes a través de los oficios; siempre bajo la luz de “Amereida”, poema épico que revela una visión poética de América.</p>
+                                <p class='gris-claro'>El territorio se ubica en en un sector de Ritoque llamado Punta Piedra, entre las comunas de Concón y Quintero en Valparaíso, Chile, a 4 kilómetros al norte de la desembocadura del río Aconcagua. Su extensión aproximada es de 240 hectáreas. Aparte de contar con un valor arquitectónico único, Ciudad Abierta alberga una biodiversidad en su ecosistema.</p>
+                                <p class='gris-claro'>El nombre ‘Ciudad Abierta’ tiene uno de sus orígenes en la película ‘Roma ciudad abierta’ (Roma città aperta, 1945) y viene de la idea de ciudad desprotegida, en la cuál no existe una fortaleza construida, la fortaleza que la defiende se construye por medio de la hospitalidad y la palabra.</p>
+<iframe width='100%' height='500px' frameBorder='0' src='https://a.tiles.mapbox.com/v3/idar.im6f61im/attribution,zoompan,geocoder.html'></iframe>
+                            </div>
+                        </div>
+                    </article> 
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class='fila'>
+    <div class='fondo-blanco'>
+        <div class='wrap'>
+            <div class='historia'>
+                <h1 class='rojo-claro'>Pontificia Universidad Católica de Valparaíso, Casa Central</h1>
+                <h4 class='subtitulo blanco'>Km.4 Camino a Concón - Quintero, Sector Punta Piedra</h4>
+                <div class='bloque'>
+                    <article class="h-entry especifico">
+                        <div class="e-content p-summary p-name">
+                            <div class='bloque'>
+                                <p>El 21 de septiembre del año 1925, era puesta la primera piedra de la que hoy es una de las instituciones de mayor trayectoria y prestigio de la educación superior chilena. Ese día nacía la Universidad Católica de Valparaíso.</p>
+                                <p>Los inicios de nuestra Casa de Estudios fueron posibles gracias a la generosidad de doña Isabel Caces de Brown, dama porteña que junto a sus hijas, señoras Isabel Brown de Brunet y María Teresa Brown de Ariztía, destacan por su trascendencia en la historia de Valparaíso.</p>
+                                <p>Vinculada a la Iglesia a través de la diócesis de Valparaíso, la Universidad ha tenido ocho Grandes Cancilleres. Cabe destacar que tres de ellos, Mons. Raúl Silva Henríquez, Mons. Jorge Medina Estévez y Mons. Francisco Javier Errázuriz, han sido investidos Cardenales.</p>
+                                <p>Desde el inicio de sus actividades académicas, en Marzo de 1928, la Universidad Católica de Valparaíso ha desarrollado una ininterrumpida labor académica, de investigación y extensión, orientando su quehacer al cultivo de las artes, ciencia y conocimiento, siempre bajo el lema legado por nuestros fundadores: Fe y Trabajo.</p>
+                                <p>Te invitamos a vivir la Universidad. A sentirla como un espacio en que te ayudaremos a potenciar y desarrollar tus habilidades e intereses para alcanzar tu plenitud como persona. Te invitamos a participar y ser parte de una institución con tradición y excelencia, que conoce los desafíos de los nuevos tiempos y se compromete con el desarrollo y futuro del país.</p>
+                                <iframe width='100%' height='500px' frameBorder='0' src='https://a.tiles.mapbox.com/v3/idar.im6g17o8/attribution,zoompan,geocoder.html'></iframe>
+                            </div>
+                        </div>
+                    </article> 
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
         </div>
     </div>
 </div>
