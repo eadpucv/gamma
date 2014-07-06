ed5a6590a303b53d561d3f66a4bafbbdc98a65f5
Juan Antonio <godoyjuanantonio@gmail.com>
2014-07-05 21:02:55 -0400

    Avance
    Trabajo en publicaciones por carrera

--- comments.php
+++ comments.php
@@ -18,7 +18,7 @@
 			<?php endif; ?>
 		</div>
 		<?php if ( comments_open() ) : ?>
-			<div class='col-lg-6 col-md-6 col-sm-12 col-xs-12'>
+			<div class='col-lg-6 col-lg-offset-1 col-md-6 col-sm-12 col-xs-12'>
 				<?php 
 					$fields =  array(
 					  'author' =>
