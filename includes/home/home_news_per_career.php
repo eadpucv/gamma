36d2bb985dd94c5a4deb1d041b7e53c5b7b89609
Juan Antonio <godoyjuanantonio@gmail.com>
2014-07-07 19:00:13 -0400

    Casi listo Noticias x carrera (gráfica)

--- includes/home/home_news_per_career.php
+++ includes/home/home_news_per_career.php
@@ -22,8 +22,8 @@
               <div class='seguir-leyendo-cuadro'>
                 <p class='seguir-leyendo'>Seguir leyendo</p><p class='seguir-leyendo ver-mas'>+</p>
               </div>
+              <a href='#'><h6>arquitectura</h6></a>
               <div class='pagina md carrera'>
-                <a href='#'><h6>arquitectura</h6></a>
                 <h4 class='blanco'><?php the_title(); ?></h4>
                 <aside class='blanco entry-details'>Publicado el  <?php the_time("d")?> de <?php the_time("F, Y") ?></aside>
                 <p class='blanco'><?php echo get_the_excerpt(); ?></p>
@@ -40,8 +40,8 @@
               <div class='seguir-leyendo-cuadro'>
                 <p class='seguir-leyendo'>Seguir leyendo</p><p class='seguir-leyendo ver-mas'>+</p>
               </div>
+              <a href='#'><h6>diseño gráfico</h6></a>
               <div class='pagina md carrera'>
-                <a href='#'><h6>diseño gráfico</h6></a>
                 <h4 class='blanco'><?php the_title(); ?></h4>
                 <aside class='blanco entry-details'>Publicado el  <?php the_time("d")?> de <?php the_time("F, Y") ?></aside>
                 <p class='blanco'><?php echo get_the_excerpt(); ?></p>
@@ -57,8 +57,8 @@
               <div class='seguir-leyendo-cuadro'>
                 <p class='seguir-leyendo'>Seguir leyendo</p><p class='seguir-leyendo ver-mas'>+</p>
               </div>
+              <a href='#'><h6>diseño industrial</h6></a>
               <div class='pagina md carrera'>
-                <a href='#'><h6>diseño industrial</h6></a>
                 <h4 class='blanco'><?php the_title(); ?></h4>
                 <aside class='blanco entry-details'>Publicado el  <?php the_time("d")?> de <?php the_time("F, Y") ?></aside>
                 <p class='blanco'><?php echo get_the_excerpt(); ?></p>
