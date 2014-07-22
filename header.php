59ef60d6e4c57e36f905ec2ea871cd54875cbe63
Idar <idargonzalez@gmail.com>
2014-07-22 18:35:51 -0400

    Ajustes en función del contenido
    1. Se revisan páginas de contenido específico como amereida, historia y
    campus y se editan pequeñas variables.

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
 
   <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
   <?php wp_head(); ?>
-  <script async type="text/javascript" src="<?php bloginfo('template_directory') ?>/js/ead-bar/ead-bar.min.js"></script> 
+  <script type="text/javascript" src="<?php bloginfo('template_directory') ?>/js/ead-bar/ead-bar.min.js"></script> 
 </head>
 
 <body lang='ES_es' data-spy="scroll" data-target="#menu-fixed">
