d8f649101dc14e27605dacf62b79bb15a414947e
Idar <idargonzalez@gmail.com>
2014-07-23 15:39:03 -0400

    avance parte 1, miércoles 23
    1. arreglos comentarios
    2. cajas docentes historia
    3. formulario de comentar

--- page-historia.php
+++ page-historia.php
@@ -85,8 +85,8 @@
                                         </div>
                                         <div class='datos-docente historia'>
                                             <h3 class='h-sin-margen'><a href='<?php echo get_permalink(25610); ?>'>Alberto Cruz</a></h3>
-                                            <h5 class='h-sin-margen'>n. 1917 - † 2013</h5>
-                                            <h6>Arquitecto y teórico de la arquitectura, Premio Nacional de Arquitectura de 1975</h6>
+                                            <h6 class='h-sin-margen'>Arquitecto y teórico de la arquitectura, Premio Nacional de Arquitectura de 1975</h6>
+                                            <h6>n. 1917 - † 2013</h6>
                                         </div>
                                         <div class='resena-docente historia'>
                                             <p class='gris-claro'>Alberto Cruz Covarrubias, destacado arquitecto chileno y teórico de la arquitectura, nace en Santiago de Chile en 1917. Ingresa a la Facultad de Arquitectura de la Universidad Católica de Chile, donde recibe su título de arquitecto en 1939. Entre 1940 y 1942 se desempeña en una oficina profesional, asociado con los arquitectos Jorge Elton y J. López, realizando encargos de viviendas en  distintos lugares del país. Durante este último año se integra como profesor ayudante del Taller de Composición Decorativa en la Facultad de Arquitectura de la Universidad Católica de Chile. En conjunto con el arquitecto Alberto Piwonka, dan un giro al sentido y metodología de dicho taller, creando el “Curso del Espacio”, que debido a su fundamento y  desarrollo de la actividad del curso, incidiría más tarde en la modificación de la enseñanza clásica propia de la Facultad. En 1949, participa en la reestructuración total del plan de estudios de dicha escuela, y un año más tarde es nombrado Profesor de Taller Arquitectónico.</p>
@@ -98,8 +98,8 @@
                                         </div>
                                         <div class='datos-docente historia'>
                                             <h3 class='h-sin-margen'><a href='<?php echo get_permalink(143); ?>'>Godofredo Iommi</a></h3>
-                                            <h5 class='h-sin-margen'>n. 1917 - † 2001</h5>
-                                            <h6>Poeta y profesor universitario </h6>
+                                            <h6 class='h-sin-margen'>Poeta y profesor universitario </h6>
+                                            <h6>n. 1917 - † 2001</h6>
                                         </div>
                                         <div class='resena-docente historia'>
                                             <p class='gris-claro'> Estudió en la Scuola Italiana de Buenos Aires y posteriormente cursó dos años de Economía, sin llegar a terminar. En su lugar comenzó una singular trayectoria dedicada a la poesía. A los 21 años, junto a Gerardo Mello Mourao, Abdias do Nascimento, Efrain Bo, Raul Young, Napoleon López F, forman una alianza poética “La Santa Hermandad de la Orquídea”. En 1967 Iommi encabezó el movimiento de la reforma universitaria en Valparaíso, el que, en diversas variantes, se extiende al resto del país. Tras la llegada de Salvador Allende al gobierno en 1970, Iommi y sus amigos se instalaron en la localidad costera de Ritoque, al norte de Concón y fundaron la Ciudad Abierta.</p>
@@ -111,8 +111,8 @@
                                         </div>
                                         <div class='datos-docente historia'>
                                             <h3 class='h-sin-margen'><a href='<?php echo get_permalink(149); ?>'>Arturo Baeza</a></h3>
-                                            <h5 class='h-sin-margen'>n. 1927 - † 2001</h5>
-                                            <h6>Arquitecto y profesor universitario</h6>
+                                            <h6  class='h-sin-margen'>Arquitecto y profesor universitario</h6>
+                                            <h6>n. 1927 - † 2001</h6>
                                         </div>
                                         <div class='resena-docente historia'>
                                             <p class='gris-claro'>Emigró a la Quinta Región en 1952. Desde ese momento “Tuto”, como cariñosamente es recordado en la Escuela de Arquitectura y Diseño de la PUCV, comenzó a realizar sus máximas pasiones que fueron crear planos y hacer clases. Su esposa, quién hoy vive en Santiago, recuerda que Arturo era muy feliz teniendo la posibilidad de enseñar a los alumnos, las técnicas que él había aprendido. “A él le encantaba hacer clases en la UCV, porque además era muy querido por los alumnos”. Estudió en Suecia y Noruega, fue citado por la ONU, junto a otros arquitectos del mundo para construir una escuela de arquitectura en Haití. Sin embargo, debido al régimen de dictadura que estaba viviendo el país, esta propuesta sólo quedó en planes.</p>
@@ -124,8 +124,8 @@
                                         </div>
                                         <div class='datos-docente historia'>
                                             <h3 class='h-sin-margen'><a href='<?php echo get_permalink(529); ?>'>Fabio Cruz</a></h3>
-                                            <h5 class='h-sin-margen'>n. 1927 - † 2007</h5>
-                                            <h6>Arquitecto</h6>
+                                            <h6  class='h-sin-margen'>Arquitecto</h6>
+                                            <h6>n. 1927 - † 2007</h6>
                                         </div>
                                         <div class='resena-docente historia'>
                                             <p class='gris-claro'>Estudió en el colegio Colegio SS.CC. de Santiago, siendo Bachiller en Humanidades en 1944. Luego estudió arquitectura en la Escuela de Arquitectura de la P.U.C.C. (1945-1948) y en la Escuela de Arquitectura de la P.U.C.V. (1949-1950) donde se titula con la tesis Diseño Urbano para una ciudad de 25.000 habitantes (Talagante, Chile). Es miembro fundador del “Instituto de Arquitectura de la P.UCV”, junto a los profesores Alberto cruz C., Godofredo Iommi M., Miguel Eyquem A., Francisco Méndez L., Jaime Bellalta, José Vial A., y Arturo Baeza D. Dicho Instituto dará origen a la re-fundación de la Escuela de Arquitectura de la P.UCV., ocurrida en 1952. Orientación y Visión original de la arquitectura y de la enseñanza de la arquitectura, que perdura hasta hoy.</p>
@@ -137,8 +137,8 @@
                                         </div>
                                         <div class='datos-docente historia'>
                                             <h3 class='h-sin-margen'><a href='<?php echo get_permalink(25614); ?>'>Miguel Eyquem</a></h3>
-                                            <h5 class='h-sin-margen'>n. </h5>
-                                            <h6>Arquitecto</h6>
+                                            <h6 class='h-sin-margen'>Arquitecto</h6>
+                                            <h6>n. </h6>
                                         </div>
                                         <div class='resena-docente historia'>
                                             <p class='gris-claro'> 
@@ -150,8 +150,8 @@
                                         </div>
                                         <div class='datos-docente historia'>
                                             <h3 class='h-sin-margen'><a href='<?php echo get_permalink(146); ?>'>José Viál Amstrong</a></h3>
-                                            <h5 class='h-sin-margen'>n. 1917 - † 1983</h5>
-                                            <h6>Arquitecto y profesor universitario</h6>
+                                            <h6 class='h-sin-margen'>Arquitecto y profesor universitario</h6>
+                                            <h6>n. 1917 - † 1983</h6>
                                         </div>
                                         <div class='resena-docente historia'>
                                             <p class='gris-claro'> Sus estudios primarios y secundarios los realizó en el colegio de los Padres Alemanes de Santiago. Luego estudió de Arquitectura en la Universidad Católica de Chile, donde ingresa en 1944. Estando en quinto año participa activamente en el movimiento que culminó con el cambio del plan de estudios de dicha Escuela. Forma parte del grupo encabezado por Alberto Cruz y el poeta Godofredo Iommi, que en 1952, funda el Instituto de Arquitectura de la Universidad Católica de Valparaíso, dependiente directamente de la Rectoría. Este Instituto asume paulatinamente la orientación y docencia de la Escuela de Arquitectura de esa Universidad.</p>
@@ -163,8 +163,8 @@
                                         </div>
                                         <div class='datos-docente historia'>
                                             <h3 class='h-sin-margen'><a href='#'>Francisco Méndez</a></h3>
-                                            <h5 class='h-sin-margen'>n. 1922 - </h5>
-                                            <h6>Arquitecto, escenógrafo, artista digital y pintor</h6>
+                                            <h6 class='h-sin-margen'>Arquitecto, escenógrafo, artista digital y pintor</h6>
+                                            <h6>n. 1922 - </h6>
                                         </div>
                                         <div class='resena-docente historia'>
                                             <p class='gris-claro'>Estudió Arquitectura en la Pontificia Universidad Católica de Chile y en la facultad de Bellas Artes de la Universidad de Chile donde fue alumno de Carlos Pedraza.vivió durante 10 años en Francia entre los años 1959 y 1969 durante los cuales fue discípulo de Henry Goetz en París, Francia y del artista holandés George Vantongerloo del movimiento Stjl o style, quien influyó directamente en su obra. Durante su permanencia en Europa realizó trabajos de escenógrafo siendo autor de numerosos montajes teatrales en París. A su regresó a Chile ocupó la Cátedra de Artes Plásticas en el Instituto de Arte de la Universidad Católica de  Valparaíso que ayudó a fundar y  de la cual llegó a ser director desde 1971 a 1986. Al regresar a Chile Méndez participó en proyecto Amereida junto al arquitecto Alberto Cruz, el poeta Godofredo Iommi, el escultor Claudio Girola y otros profesionales arquitectos y artistas. Experimentó con la interpretación del paisaje de América realizando murales en la ciudad de Valparaíso entre 1969 y 1973 en los contrafuertes de los cerros. Junto a los integrantes de Amereida  fue parte de la fundación de la Ciudad Abierta de Ritoque en la V región en 1970.</p>
@@ -176,8 +176,8 @@
                                         </div>
                                         <div class='datos-docente historia'>
                                             <h3 class='h-sin-margen'><a href='#'>Jaime Bellalta</a></h3>
-                                            <h5 class='h-sin-margen'>n.  - † 2007</h5>
-                                            <h6>Arquitecto y profesor universitario </h6>
+                                            <h6 class='h-sin-margen'>Arquitecto y profesor universitario </h6>
+                                            <h6>n.  - † 2007</h6>
                                         </div>
                                         <div class='resena-docente historia'>
                                             <p class='gris-claro'>Nacido en Santiago de Chile, Bellalta estudió en la Pontificia Universidad Católica de Chile, Graduate School of Design de la Universidad de Harvard y la Universidad de Londres. Se unió a la facultad de la Pontificia Universidad Católica en 1968, impartió la enseñanza de la arquitectura y el diseño urbano allí hasta 1975. Durante esos años también practica en privado y se desempeñó como director de la Agencia Nacional de Renovación Urbana de Chile y como director ejecutivo de la Academia de Humanismo Cristiano. Su galardonado diseño para el monasterio benedictino de Las Condes, Santiago, ha sido nombrado uno de los monumentos arquitectónicos históricos nacionales de Chile.</p>
@@ -189,8 +189,8 @@
                                         </div>
                                         <div class='datos-docente historia'>
                                             <h3 class='h-sin-margen'><a href='<?php echo get_permalink(150); ?>'>Claudio Girola</a></h3>
-                                            <h5 class='h-sin-margen'>n. 1923 - † 1994</h5>
-                                            <h6>Escultor y artista visual</h6>
+                                            <h5 class='h-sin-margen'>Escultor y artista visual</h5>
+                                            <h6>n. 1923 - † 1994</h6>
                                         </div>
                                         <div class='resena-docente historia'>
                                             <p class='gris-claro'>Perteneciente a una familia de connotados artistas, su formación comenzó junto a su padre quien era escultor cincelador. Estudió en Argentina en la Escuela Preparatoria de Bellas Artes Manuel Belgrano y en la Academia Nacional de Bellas Artes de Buenos Aires de la que se retiró en el año 1943, tras oponerse a la educación artística imperante en ese entonces. En 1946 presentó su primera exposición como integrante del grupo Arte Concreto junto a los artistas Alfredo Hlito y Tomás Maldonado. En 1949 viajó a Europa, residió en París y Milán donde expuso y perfeccionó sus conocimientos con el artista Georges Vatongerloo. A su regreso a Argentina expuso obras abstractas junto al grupo Artistas Modernos de Argentina. Se radicó en Chile en 1952 y se integró como docente de la Escuela de Arquitectura de la Universidad Católica de Valparaíso. </p>
