6ee46273c16c40771ba9299cbe642aad54e9e018
Idar <idargonzalez@gmail.com>
2014-08-01 13:29:26 -0400

    ajustes de mapa + parallax

--- page-campus.php
+++ page-campus.php
@@ -22,7 +22,7 @@
                             <div class='bloque'>
                                 <p>La sede de la Escuela de Arquitectura y Diseño se encuentra ubicada en la calle Matta, Recreo. La casa original fue construida alrededor de 1940 con fines residenciales conforme a su primera recepción municipal y fue adquirida por la Universidad en el año 1951 bajo la rectoría del R.P. Jorge González, quien en su momento dijo que...“la ubicación está muy de acuerdo con los ideales de un arquitecto que debe ser un hombre que tenga visión amplia y que mire el mar”. Hasta el día de hoy el edificio mantiene la tipología de una residencia de estilo neocolonial y sus recintos han sido alterados conforme a los requerimientos educacionales. Asímismo, durante los años 90-95 se realizó una serie de transformaciones en la propiedad de Matta 12, que incluyó modificaciones en sus espacios exteriores. </p>
                                 <p>Cabe señalar que el espacio físico para esta Escuela ha sido materia de estudio permanentemente, inherente al oficio de los arquitectos y diseñadores que han encontrado, en distintas oportunidades, un campo para la realización de obras que plantean problemáticas innovadoras, propias al oficio.</p>
-                                <iframe width='100%' height='300px' frameBorder='0' src='https://a.tiles.mapbox.com/v3/idar.im6c5nep/attribution,zoompan,geocoder.html'></iframe>
+                                <iframe width="100%" height="250px" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=calle%20matta%2012%2C%20recreo&key=AIzaSyCZiilPUQ_vIqpLmZk_H_2sCcihjCBdwwE"></iframe> 
                             </div>
                         </div>
                     </article> 
@@ -42,8 +42,7 @@
                             <div class='bloque'>
                                 <p class='gris-claro'>La Escuela es la autora y principal protagonista, aunque no la única, del proyecto denominado “Ciudad Abierta”. En lo fundamental, es una extensión de 270 hectáreas ubicada 20 kilómetros al norte de Valparaíso, fundada en 1970 por poetas, arquitectos, diseñadores, escultores y filósofos, como una utopía que permite vincular vida, trabajo y estudio en “la plenitud y libertad de los oficios”.</p>
                                 <p class='gris-claro'>Hoy la Ciudad Abierta está íntimamente vinculada a la Escuela, y es habitada por algunos de sus profesores y otros adherentes. Ella está constituida como una corporación cultural sin fines de lucro, que da cabida a obras de arquitectura y diseño de carácter experimental y otras actividades académicas en que participan los alumnos constantemente.</p>
-                                <iframe width='100%' height='300px' frameBorder='0' src='https://a.tiles.mapbox.com/v4/idar.im6f61im/attribution,zoompan,share.html?access_token=pk.eyJ1IjoiaWRhciIsImEiOiJQNGFvOVdVIn0.7FwJUgWdhWOSOXt-jdtnUw'></iframe>
-                            </div>
+                                <iframe width="100%" height="350" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=-32.888817%2C%20-71.504260&key=AIzaSyCZiilPUQ_vIqpLmZk_H_2sCcihjCBdwwE"></iframe> 
                         </div>
                     </article> 
                 </div>
@@ -66,7 +65,7 @@
                                 <p>Vinculada a la Iglesia a través de la diócesis de Valparaíso, la Universidad ha tenido ocho Grandes Cancilleres. Cabe destacar que tres de ellos, Mons. Raúl Silva Henríquez, Mons. Jorge Medina Estévez y Mons. Francisco Javier Errázuriz, han sido investidos Cardenales.</p>
                                 <p>Desde el inicio de sus actividades académicas, en Marzo de 1928, la Universidad Católica de Valparaíso ha desarrollado una ininterrumpida labor académica, de investigación y extensión, orientando su quehacer al cultivo de las artes, ciencia y conocimiento, siempre bajo el lema legado por nuestros fundadores: Fe y Trabajo.</p>
                                 <p>Te invitamos a vivir la Universidad. A sentirla como un espacio en que te ayudaremos a potenciar y desarrollar tus habilidades e intereses para alcanzar tu plenitud como persona. Te invitamos a participar y ser parte de una institución con tradición y excelencia, que conoce los desafíos de los nuevos tiempos y se compromete con el desarrollo y futuro del país.</p>
-                                <iframe width='100%' height='500px' frameBorder='0' src='https://a.tiles.mapbox.com/v3/idar.im6g17o8/attribution,zoompan,geocoder.html'></iframe>
+                                <iframe width="100%" height="350" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=avenida%20brasil%202950%2C%20valparaiso%2C%20chile&key=AIzaSyCZiilPUQ_vIqpLmZk_H_2sCcihjCBdwwE"></iframe> 
                             </div>
                         </div>
                     </article> 
