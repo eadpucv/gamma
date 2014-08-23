5f21bac5d38b655794330c61214a17544a1fb0ae
Rodrigo Moya <rodrigo@citisent.com>
2014-08-23 20:39:43 -0300

    scrollspy

--- header.php
+++ header.php
@@ -20,6 +20,7 @@
   <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
   <?php wp_head(); ?>
   <script type="text/javascript" src="<?php bloginfo('template_directory') ?>/js/jquery.js "></script>
+  <script type="text/javascript" src="<?php bloginfo('template_directory') ?>/js/scrollspy.js"></script>  
 </head>
 
 <body data-spy="scroll" data-target="#menu-fixed">
