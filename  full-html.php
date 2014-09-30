bf055c8c82749445adf9a98b1a692602aab274f7
Rodrigo Moya <rodrigo@citisent.com>
2014-09-30 20:40:33 -0300

    full-html template

---
+++  full-html.php
@@ -0,0 +1,14 @@
+<?php 
+/*
+Template Name: Full-html
+*/
+?>
+
+<?php get_header();  ?>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<?php the_content(); ?>
+<?php endwhile; else: ?>
+  	<p>Sorry, no posts matched your criteria.</p>
+<?php endif; ?>
+<?php get_footer(); ?>
+?>
